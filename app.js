f86cec057b9f719104fad123602c255ca0e4605a
Allan Assis <allan.andrade@corp.globo.com>
2019-09-14 23:42:31 -0300

    refactor: moving all fastify instance to children process

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 const cluster = require("cluster");
-const fastify = require("fastify")();
 const voteRotes = require("./routes/votes");
+const { client } = require("./models/Redis");
+
 const NUM_CPU_CORES = require("os").cpus().length;
 const port = 3000;
-
-fastify.register(require("fastify-redis"), { host: "127.0.0.1" });
-fastify.register(require("fastify-mongodb"), {
-  forceClose: true,
-  useUnifiedTopology: true,
-  url: "mongodb://127.0.0.1/bbb"
-});
 
 if (cluster.isMaster) {
   for (let i = 0; i < NUM_CPU_CORES; i++) {
     cluster.fork();
   }
-  cluster.on("exit", worker => {
+  cluster.on("exit", _ => {
     cluster.fork();
   });
 } else {
-  fastify.register(voteRotes);
+  const fastify = require("fastify")();
+  fastify.register(require("fastify-redis"), {
+    client,
+    host: "127.0.0.1"
+  });
+  fastify.register(require("fastify-mongodb"), {
+    forceClose: true,
+    useUnifiedTopology: true,
+    url: "mongodb://127.0.0.1/bbb"
+  });
+  fastify.register(voteRotes(cluster.worker));
+
+  console.log(cluster.worker.id);
   fastify.listen(port, () => {
     console.log(
       `Fastify "Hello World" listening on port ${port}, PID: ${process.pid}`
