2469958e3a78dfe94d597fc1a983de5b2bfe068d
Allan Assis <allan.andrade@corp.globo.com>
2019-09-21 10:18:46 -0300

    refactor: saving all votes in memory with redis

--- src/routes/votes.js
+++ src/routes/votes.js
@@ -1,30 +1,15 @@
-const Queue = require("../models/Queue");
-const cluster = require("cluster");
-module.exports = (fastify, opts, next) => {
-  const {
-    redis,
-    mongo: { client: mongoClient }
-  } = fastify;
-  const mongoDB = mongoClient.db("bbb");
-  const voteQueue = new Queue(redis);
+const Vote = require("../models/Votes");
 
+const cluster = require("cluster");
+
+module.exports = (fastify, opts, next) => {
   fastify.post("/", async ({ body: data }, reply) => {
     console.log(cluster.worker.id);
     const { id } = data;
-    const recordsLen = await voteQueue.size(id);
-
-    if (recordsLen == 100) {
-      await incVotes(recordsLen, id).then(_ => voteQueue.clean(id));
-    }
-    voteQueue.enqueue(id, data);
+    const votes = new Vote(id);
+    votes.incVote();
     reply.res.end();
   });
-
-  function incVotes(n, id) {
-    return mongoDB
-      .collection("bigwall")
-      .updateOne({ id }, { $inc: { votes: n } }, { upsert: true });
-  }
 
   next();
 };
