0c2dfb84a94b963077e1d0a76b23f337f6b7ed2a
Allan Assis <allan.andrade@corp.globo.com>
2019-09-21 11:56:36 -0300

    refactor: add private method do return mongo client instance

--- src/models/Mongo.js
+++ src/models/Mongo.js
@@ -1,14 +1,28 @@
-const Mongo = require("mongodb").MongoClient;
+const Mongo = require('mongodb').MongoClient;
 
 class MongoClient {
   constructor({ url, dbName, ...rest }) {
-    this.client = Mongo(url, rest);
+    this.client = new Mongo(`${url}/${dbName}`, rest);
+    this.url = url;
+    this.sdbName = dbName;
+  }
+  async connect() {
+    return await this._connect();
+  }
+
+  _connect(){
+    return new Promise((resolve, reject) => {
+      this.client.connect((err, client) => {
+        if(err) reject(err)
+        resolve(client)
+      })
+    })
   }
 }
 
 module.exports = new MongoClient({
-  url: "mongodb://127.0.0.1",
-  dbName: "bbb",
+  url: 'mongodb://127.0.0.1',
+  dbName: 'bbb',
   useUnifiedTopology: true,
   useNewUrlParser: true
 });
