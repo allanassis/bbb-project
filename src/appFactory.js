b253b6b8de43ba8e7f3e7d05b658b376f6223dab
Allan Assis <allan.andrade@corp.globo.com>
2019-09-21 10:35:52 -0300

    refactor: allowed null db config for tests

--- src/appFactory.js
+++ src/appFactory.js
@@ -1,16 +1,18 @@
-const fastify = require("fastify");
-const fastifyMongo = require("fastify-mongodb");
-const fastifyRedis = require("fastify-redis");
+const fastify = require('fastify');
+const fastifyMongo = require('fastify-mongodb');
+const fastifyRedis = require('fastify-redis');
 
-const createApp = (config, routes, dbsConfig) => {
+const createApp = (config, routes, dbsConfig = null) => {
   const fastifyInstance = fastify(config);
   // Registando os bancos de dados
-  fastifyInstance.register(fastifyRedis, { ...dbsConfig.redis });
-  fastifyInstance.register(fastifyMongo, { ...dbsConfig.mongo });
+  if (dbsConfig && dbsConfig.mongo && dbsConfig.redis) {
+    fastifyInstance.register(fastifyRedis, { ...dbsConfig.redis });
+    fastifyInstance.register(fastifyMongo, { ...dbsConfig.mongo });
+  }
 
   routes.forEach(route => fastifyInstance.register(route));
 
-  return fastifyInstance
+  return fastifyInstance;
 };
 
 module.exports = createApp;
