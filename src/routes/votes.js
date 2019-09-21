4da7adebb386ddedf9a996c6fe537d84ee93b595
Allan Assis <allan.andrade@corp.globo.com>
2019-09-21 10:48:18 -0300

    feat: add get vote route

--- src/routes/votes.js
+++ src/routes/votes.js
@@ -1,14 +1,21 @@
-const Vote = require("../models/Votes");
+const Vote = require('../models/Votes');
 
-const cluster = require("cluster");
+const cluster = require('cluster');
 
 module.exports = (fastify, opts, next) => {
-  fastify.post("/vote", async ({ body: data }, reply) => {
+  fastify.post('/vote', async ({ body: data }, reply) => {
     console.log(cluster.worker.id);
     const { id } = data;
     const votes = new Vote(id);
     votes.incVote();
-    reply.res.end();
+    reply.send({id, msg: "Voto realizado com sucesso"});
+  });
+
+  fastify.get('/vote/:id', async (req, reply) => {
+    const { id } = req.params;
+    const vote = new Vote(id);
+    const numVotes = await vote.getVote();
+    reply.send({ id, numVotes });
   });
 
   next();
