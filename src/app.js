d6b43b1589394cb0520c2f2cda99f50623d8a291
Allan Assis <allan.andrade@corp.globo.com>
2019-09-21 10:15:26 -0300

    refactor: persist votes on disk in background

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const { client } = require("./models/Redis");
 const { client: mongoClient } = require("./models/Mongo");
 
 const NUM_CPU_CORES = require("os").cpus().length;
-const port = 3000;
 
 if (cluster.isMaster) {
   for (let i = 0; i < NUM_CPU_CORES; i++) {
@@ -14,8 +13,13 @@ if (cluster.isMaster) {
   cluster.on("exit", _ => {
     cluster.fork();
   });
+  setInterval(() => {
+    console.log(`salvando...`);
+    const save = client.bgsave();
+    console.log(save)
+  }, 20000);
 } else {
-  mongoClient.connect((err => {
+  mongoClient.connect(err => {
     createApp({}, [voteRotes], {
       mongo: {
         client: mongoClient
@@ -25,8 +29,7 @@ if (cluster.isMaster) {
         host: "127.0.0.1"
       }
     });
-
-  }))
+  });
 
   console.log(cluster.worker.id);
 }
